c10481e828aa618b5c7fd8f63092cbb8ea018107
rizqi dimas setiawan <rizqdimas1234@gmail.com>
2021-11-03 23:32:34 +0700

    finishing in crud in api

---
+++ crud-in-api/app/Http/Controllers/API/ProgramController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Program;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ProgramResource;
+
+class ProgramController extends Controller
+{
+    public function index()
+    {
+        $data = Program::latest()->get();
+        return response()->json([
+            ProgramResource::collection($data),
+            'Programs fetched.',
+        ]);
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'address' => 'required|string|max:255',
+            'description' => 'required',
+            'category' => 'required|string|max:255',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+        $program = Program::create([
+            'name' => $request->name,
+            'address' => $request->address,
+            'description' => $request->description,
+            'category' => $request->category,
+        ]);
+
+        return response()->json([
+            'Program created successfully.',
+            new ProgramResource($program),
+        ]);
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $program = Program::find($id);
+        if (is_null($program)) {
+            return response()->json('Data not found', 404);
+        }
+        return response()->json([new ProgramResource($program)]);
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Program $program)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'address' => 'required|string|max:255',
+            'description' => 'required',
+            'category' => 'required|string|max:255',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+        $program->name = $request->name;
+        $program->address = $request->address;
+        $program->description = $request->description;
+        $program->category = $request->category;
+        $program->save();
+
+        return response()->json([
+            'Program updated successfully.',
+            new ProgramResource($program),
+        ]);
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Program $program)
+    {
+        $program->delete();
+        return response()->json('Program deleted successfully');
+    }
+}
--- crud-in-api/routes/api.php
+++ crud-in-api/routes/api.php
@@ -14,6 +14,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/register', [
+    App\Http\Controllers\API\AuthController::class,
+    'register',
+]);
+//API route for login user
+Route::post('/login', [
+    App\Http\Controllers\API\AuthController::class,
+    'login',
+]);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+//Protecting Routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/profile', function (Request $request) {
+        return auth()->user();
+    });
+    Route::resource(
+        'programs',
+        App\Http\Controllers\API\ProgramController::class
+    );
+    // API route for logout user
+    Route::post('/logout', [
+        App\Http\Controllers\API\AuthController::class,
+        'logout',
+    ]);
 });
